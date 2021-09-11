46e440482ad1d76ddd5e9a1efd76a63f899fd4fa
kopch02 <kopch08@gmail.com>
2021-09-11 13:56:56 +0700

    new

--- 3semestr/1lab/1lab.cpp
+++ 3semestr/1lab/1lab.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
 struct datetime
 {
-    int number;
     int second;
     int minute;
     int hour;
@@ -14,317 +14,165 @@ struct datetime
     int month;
     int year;
 };
-struct DLList
+
+void in(datetime* array,int n)
 {
-    
-    struct Node
-    {
-        datetime data;     //information
-        Node* next;   //next node position
-        Node* prev;   //previous node position
-    };
-
-    Node* F=NULL;     //first node
-    Node* L=NULL;     //last node
-    Node* C=NULL;     //current node
-    int Count;        //node count
-
-    void Out();
-    void Info();
-    void Clear();
-
-    bool MoveNext();
-    bool MovePrev();
-    bool MoveFirst();
-    bool MoveLast();
-
-    bool Init(datetime);
-    bool AddNext(datetime);
-    bool AddPrev(datetime);
-    bool AddFirst(datetime);
-    bool AddLast(datetime);
-
-    bool Del(datetime&);
-    bool DelNext(datetime&);
-    bool DelPrev(datetime&);
-    bool DelFirst(datetime&);
-    bool DelLast(datetime&);
-};
-
-bool DLList::MoveNext()
-{
-    if(!F)       return false;
-    if(!C)       {C=F; return true;}
-    if(C==L) return false;
-    C=C->next;
-    return true;
-}
-
-bool DLList::MovePrev()
-{
-    if(!F)       return false;
-    if(!C)       {C=F; return true;}
-    if(!C->prev) return false;
-    C=C->prev;
-    return true;
-}
-bool DLList::MoveFirst()
-{
-    if(!F) return false;
-    C=F;
-    return true;
-}
-
-bool DLList::MoveLast()
-{
-    if(!L) return false;
-    C=L;
-    return true;
-}
-
-bool DLList::Init(datetime data)
-{
-   if(!F)
-   {
-        F=new Node;
-        L=F;
-        C=F;
-        F->prev=NULL;
-        F->next=NULL;
-        F->data = data;
-        Count=1;
-        return true;
-   }
-   else
-        return false;
-}
-
-bool DLList::AddNext(datetime data)
-{
-	if(!F) return Init(data);
-
-	Node* temp=C->next;
-	C->next=new Node;
-	C->next->next=temp;
-	C->next->prev=C;
-
-	if(!temp)
-        L=C->next;
-	else
-        temp->prev=C->next;
-
-    C=C->next;
-    C->data=data;
-    Count++;
-    return true;
-
-}
-
-bool DLList::AddPrev(datetime data)
-{
-	if(!F) return Init(data);
-
-	Node* temp=C->prev;
-	C->prev=new Node;
-	C->prev->next=C;
-	C->prev->prev=temp;
-
-	if(!temp)
-        F=C->prev;
-	else
-        temp->next=C->prev;
-
-    C=C->prev;
-    C->data=data;
-    Count++;
-    return true;
-}
-
-bool DLList::AddFirst(datetime data)
-{
-    if (MoveFirst())return AddPrev(data);
-    else return false;
-}
-
-bool DLList::AddLast(datetime data)
-{
-    if (MoveLast())return AddNext(data);
-    else return false;
-}
-
-void DLList::Out()
-{
-    if(!F)  {
-                cout<<"List is empty"<<endl;
-                return;
-            }
-
-    Node* temp=C;
-    cout<<"List: "<<endl;
-    
-    
-        cout<<endl;
-        cout<<"--------------"<<endl;
-        cout<<endl;
-        cout<<"number - "<<temp->data.number<<endl;
-       cout<<"second - "<<temp->data.second<<endl;
-       cout<<"minute - "<<temp->data.minute<<endl;
-       cout<<"hour - "<<temp->data.hour<<endl;
-       cout<<"day - "<<temp->data.day<<endl;
-       cout<<"month - "<<temp->data.month<<endl;
-       cout<<"year - "<<temp->data.year<<endl;
-    
- 
-    cout<<endl;
-}
-
-void DLList::Info()
-{
-
-cout<<endl;
-cout<<endl;
-    if(Count)
-        cout<<"List node count: "<<Count<<endl;
-    else
-        cout<<"List is empty"<<endl;
-   bool flag = true;
-    if(!C)
-    {
-        flag=MoveFirst();
-    }
-    Node* temp=C;
-    cout<<endl;
-        temp=temp->next;
-    for (int i=0;i<2;i++)
-    {
-        cout<<"Current node data = "<<endl;
-        cout<<endl;
-        cout<<"number - "<<temp->data.number<<endl;
-        cout<<"second - "<<temp->data.second<<endl;
-        cout<<"minute - "<<temp->data.minute<<endl;
-        cout<<"hour - "<<temp->data.hour<<endl;
-        cout<<"day - "<<temp->data.day<<endl;
-        cout<<"month - "<<temp->data.month<<endl;
-        cout<<"year - "<<temp->data.year<<endl;
-        temp=temp->prev;
-    }
-cout<<endl;
-cout<<endl;
-
-}
-
-bool DLList::DelFirst(datetime& data)
-{
-    if(!F)      return false;
-    if(C!=F)    MoveFirst();
-
-    Node* temp = C->next;
-    data = C->data;
-
-    if(temp) temp->prev=NULL;
-    delete C;
-
-    C=temp;
-    F=temp;
-    Count--;
-    if(!temp) {L=NULL; return false;}
-    return true;
-}
-
-bool DLList::DelLast(datetime& data)
-{
-    if(!F)      return false;
-    if(C!=L)    MoveLast();
-
-    Node* temp = C->prev;
-    data = C->data;
-
-    if(temp) temp->next=NULL;
-    delete C;
-
-    C=temp;
-    L=temp;
-    Count--;
-
-    if(!temp) {F=NULL; return false;}
-    return true;
-}
-
-bool DLList::Del(datetime& data)
-{
-    if(!F) return false;
-	if(!C) return false;
-
-	if(C==F) return DelFirst(data);
-    if(C==L) return DelLast(data);
-
-    Node* temp = C->next;
-	data=C->data;
-
-	C->prev->next=C->next;
-	C->next->prev=C->prev;
-
-	delete C;
-	C=temp;
-	Count--;
-	return true;
-}
-
-bool DLList::DelNext(datetime& data)
-{
-    if(!F) return false;
-	if(!C) return false;
-	if (MoveNext()) return Del(data);
-    return false;
-}
-bool DLList::DelPrev(datetime& data)
-{
-    if(!F) return false;
-	if(!C) return false;
-	if (MovePrev()) return Del(data);
-    return false;
-}
-
-void DLList::Clear()
-{
-   if(!MoveFirst())
-   {
-       cout<<"List is empty"<<endl;
-       return;
-   }
-   datetime k;
-   while(Del(k));
-}
-
-int main()
-{
-    DLList MyList;
-
-    datetime a;
-    datetime buff;
-
     ifstream file;
-    ofstream ff;
-    
     file.open("file.txt");
-    for(int i=0;i<4;i++)
+    for (int i=0;i<n;i++)
     {
-    file>>a.number>>a.second>>a.minute>>a.hour>>a.day>>a.month>>a.year;
-    MyList.AddNext(a);
+    file>>array[i].second>>array[i].minute>>array[i].hour>>array[i].day>>array[i].month>>array[i].year;
     }
     file.close();
-    MyList.MovePrev();
-    cout<<endl<<"current"<<endl<<endl;
-    MyList.Out();
-    MyList.MoveNext();
-    cout<<"next"<<endl<<endl;
-    MyList.Out();
-    MyList.MovePrev();
-    MyList.MovePrev();
-    cout<<"prev"<<endl<<endl;
-    MyList.Out();
+}
+void out(datetime* array,int i)
+{
+    //for(int i=0;i<1;i++)
     
+        cout<<"second"<<setw(7)<<"minute"<<setw(5)<<"hour"<<setw(5)<<"day"<<setw(8)<<"month"<<setw(7)<<"year"<<setw(5)<<endl;
+        cout<<array[i].second<<setw(7)<<array[i].minute<<setw(5)<<array[i].hour<<setw(5)<<array[i].day<<setw(7)<<array[i].month<<setw(8)<<array[i].year;
+        cout<<endl;
+    
+}
+void nextdate(datetime* array,int i)
+{
+    
+    if ((array[i].month==4)||(array[i].month==6)||(array[i].month==9)||(array[i].month==11))
+    {
+        if (array[i].day==30)
+        {
+            array[i].day=1;
+            array[i].month+=1;
+            if(array[i].month==13)
+            {
+                array[i].month=1;
+                array[i].year+=1;
+            }
+        }
+        else{
+            array[i].day+=1;
+        }
+        return;
+    }
+    if(array[i].month==2)
+    {
+        if((array[i].year%4==0) &&(array[i].year%4000!=0) && ((array[i].year%100!=0)||(array[i].year%400==0)))
+        {
+            if(array[i].day==29)
+            {
+                array[i].day=1;
+                array[i].month+=1;
+            }
+            else{
+                array[i].day+=1;
+            }
+        }
+        else{
+            if(array[i].day==28)
+            {
+                array[i].day=1;
+                array[i].month+=1;
+            }
+            else{
+                array[i].day+=1;
+            }
+        }
+        return;
+    }
+    if((array[i].month==1)||(array[i].month==3)||(array[i].month==5)||(array[i].month==7)||(array[i].month==8)||(array[i].month==10)||(array[i].month==12))
+    {    
+        if (array[i].day==31)
+        {
+            array[i].day=1;
+            array[i].month+=1;
+            if(array[i].month==13)
+            {
+                array[i].month=1;
+                array[i].year+=1;
+            }
+        }
+        else{
+            array[i].day+=1;
+        }
+        return;
+    }
+}
+void prevdate(datetime* arrayPrev,int i)
+{
+    arrayPrev[i].day-=1;
+
+    if(arrayPrev[i].day==0)
+    {
+        arrayPrev[i].month-=1;
+        if(arrayPrev[i].month==2)
+        {
+            if((arrayPrev[i].year%4==0) &&(arrayPrev[i].year%4000!=0) && ((arrayPrev[i].year%100!=0)||(arrayPrev[i].year%400==0)))
+            {
+                arrayPrev[i].day=29;
+            }
+            else {
+                arrayPrev[i].day=28;
+            }
+            return;
+        }
+        if((arrayPrev[i].month==1)||(arrayPrev[i].month==3)||(arrayPrev[i].month==5)||(arrayPrev[i].month==7)||(arrayPrev[i].month==8)||(arrayPrev[i].month==10)||(arrayPrev[i].month==12))
+        {
+            arrayPrev[i].day=31;
+            return;
+        }
+        if(arrayPrev[i].month==0)
+        {
+            arrayPrev[i].month=12;
+            arrayPrev[i].year-=1;
+            arrayPrev[i].day=31;
+            return;
+        }
+        else 
+        {
+            arrayPrev[i].day=30;
+            return;
+        }
+
+    }
+}
 
 
+
+
+main()
+{
+    int n=8;
+    datetime date;
+    datetime* array = new datetime[n];
+    datetime* arrayPrev = new datetime[n];
+    datetime* arrayCurrent = new datetime[n];
+    in(array,n);
+    in(arrayPrev,n);
+    in(arrayCurrent,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<endl<<"current"<<endl;
+        out(array,i);
+        cout<<endl<<"next"<<endl;
+        nextdate(array,i);
+        out(array,i);
+        cout<<endl<<"prev"<<endl;
+        prevdate(arrayPrev,i);
+        out(arrayPrev,i);
+     
+    }
+    arrayPrev=arrayCurrent;
+    for(int i=0;i<n;i++)
+    {
+        if(arrayCurrent[i].day==1)
+        {
+            cout<<endl<<"dates for variant"<<endl;
+            cout<<endl<<"current"<<endl;
+            out(arrayCurrent,i);
+            cout<<endl<<"prev"<<endl;
+            prevdate(arrayPrev,i);
+            out(arrayPrev,i);
+        }
+    }
     return 0;
 }
