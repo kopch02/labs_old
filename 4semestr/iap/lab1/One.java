cc3043998885fca387c8947460465f5369d0bd38
kopch02 <kopch08@gmail.com>
2022-02-15 16:13:54 +0700

    new

--- 4semestr/iap/lab1/One.java
+++ 4semestr/iap/lab1/One.java
@@ -5,115 +5,196 @@ class One {
         Create r=new Create();
         r.run();
     }
-}
+}  
 class Create{
     public void run(){
-        Scanner in = new Scanner(System.in);
-        double S;
+        try{
+            Scanner in = new Scanner(System.in);
+            double S,V;
 
-        System.out.print("endter a and b for eleps a->");
-        double ae=in.nextDouble();
-        System.out.print("b->");
-        double be=in.nextDouble();
+            System.out.print("endter a and b for eleps a->");
+            double ae=in.nextDouble();
+            System.out.print("b->");
+            double be=in.nextDouble();
+            System.out.print("hight for eleps->");
+            double he=in.nextDouble();
 
-        System.out.print("enter radius for circle(r)->");
-        double r=in.nextDouble();
+            System.out.print("enter radius for circle(r)->");
+            double r=in.nextDouble();
+            System.out.print("hight for circle->");
+            double hc=in.nextDouble();
 
-        System.out.print("enter a and b sides of the rectangle a->");
-        double a=in.nextDouble();
-        System.out.print("b->");
-        double b=in.nextDouble();
+            System.out.print("enter a and b sides of the rectangle a->");
+            double a=in.nextDouble();
+            System.out.print("b->");
+            double b=in.nextDouble();
+            System.out.print("hight for rectangle->");
+            double hr=in.nextDouble();
 
-        System.out.print("enter a sides of the square a->");
-        double as=in.nextDouble();
+            System.out.print("enter a sides of the square a->");
+            double as=in.nextDouble();
+            System.out.print("hight for square->");
+            double hs=in.nextDouble();
 
-        System.out.print("endter base of triangle(base)->");
-        double at=in.nextDouble();
-        System.out.print("enter height for triangle(h)->");
-        double h=in.nextDouble();
+            System.out.print("endter base of triangle(base)->");
+            double at=in.nextDouble();
+            System.out.print("enter height for triangle(h)->");
+            double h=in.nextDouble();
+            System.out.print("hight for triangle->");
+            double ht=in.nextDouble();
 
-        Eleps e= new Eleps();
-        Circle c= new Circle();
-        Rectangle rec= new Rectangle();
-        Square s= new Square();
-        Triangle t= new Triangle();
+            Shape e= new Eleps(ae,be);
+            Shape c= new Circle(r);
+            Shape rec= new Rectangle(a,b);
+            Shape s= new Square(as);
+            Shape t= new Triangle(at,h);
+
+            Cylinder ce = new Cylinder(e,he);
+            Cylinder cc = new Cylinder(c,hc);
+            Cylinder cr = new Cylinder(rec,hr);
+            Cylinder cs = new Cylinder(s,hs);
+            Cylinder ct = new Cylinder(t,ht);
 
 
-        e.create_eleps(ae, be);
-        c.create_circle(r);
-        rec.create_rectangle(a, b);
-        s.create_square(as);
-        t.create_triangle(at, h);
+            S=e.area();
+            System.out.printf("площадь эллипса= %s\n",S);
+            V=ce.volume();
+            System.out.printf("обьём эллипса= %s\n\n",V);
 
-        S=e.area();
-        System.out.printf("площадь эллипса= %s\n",S);
+            S=c.area();
+            System.out.printf("площадь круга= %s\n",S);
+            V=cc.volume();
+            System.out.printf("обьём круга= %s\n\n",V);
 
-        S=c.area();
-        System.out.printf("площадь круга= %s\n",S);
+            S=rec.area();
+            System.out.printf("площадь прямоугольника= %s\n",S);
+            V=cr.volume();
+            System.out.printf("обьём прямоугольника= %s\n\n",V);
 
-        S=rec.area();
-        System.out.printf("площадь прямоугольника= %s\n",S);
+            S=s.area();
+            System.out.printf("площадь квадрата= %s\n",S);
+            V=cs.volume();
+            System.out.printf("обьём квадрата= %s\n\n",V);
 
-        S=s.area();
-        System.out.printf("площадь квадрата= %s\n",S);
+            S=t.area();
+            System.out.printf("площадь треугольника= %s\n",S);
+            V=ct.volume();
+            System.out.printf("обьём треугольника= %s\n\n",V);
 
-        S=t.area();
-        System.out.printf("площадь треугольника= %s\n",S);
-
-        in.close();
+            in.close();
+        }catch(IllegalArgumentException exc)
+        {
+            System.out.println(exc.getMessage());
+        }
     }
 }
-class Shape{
+interface Shape{
     final double PI=3.14159265359;
+    double area();
+    void show();
 }
-class Eleps extends Shape{
-    double A;
-    double B;
-    public void create_eleps(double ae, double be){
-        A=ae;
-        B=be;
+
+class Cylinder{
+    double S;
+    double H;
+    Cylinder(Shape a,double h){
+        if (h>0){
+            this.S=a.area();
+            this.H=h;
+        }
+        else{
+            throw new IllegalArgumentException("error Cylinder");
+        }
+    }
+    double volume(){
+        return(S*H);
+    }
+}
+
+class Eleps implements Shape{
+    protected double A;
+    protected double B;
+    public Eleps(){}
+    public Eleps(double ae, double be){
+        if ((ae>0) && (be>0)){
+            this.A=ae;
+            this.B=be;
+        }
+        else{
+            throw new IllegalArgumentException("error Eleps");
+        }
     }
     public double area(){
         return(A*B*PI);
     }
+    public void show(){
+
+    }
 }
 class Circle extends Eleps{
-    double R;
-    public void create_circle(double r){
-        R=r;
+    public Circle(double r){
+        if(r>0){
+            this.A=r;
+            this.B=r;
+        }
+        else{
+            throw new IllegalArgumentException("error Circle");
+        }
     }
-    public double area(){
-        return(R*R*PI);
+    public void show(){
+
     }
 }
-class Triangle extends Shape{
+class Triangle implements Shape{
     double A;
     double H;
-    public void create_triangle(double a, double h){
-        A=a;
-        H=h;
+    public Triangle(double a, double h){
+        if ((a>0)&&(h>0)){
+            this.A=a;
+            this.H=h;
+        }
+        else{
+            throw new IllegalArgumentException("error Triangle");
+        }
     }
     public double area(){
         return(0.5*A*H);
     }
+    public void show(){
+
+    }
 }
-class Rectangle extends Shape{
+class Rectangle implements Shape{
     double A;
     double B;
-    public void create_rectangle(double a, double b){
-        A=a;
-        B=b;
+    public Rectangle(){}
+    public Rectangle(double a, double b){
+        if ((a>0)&&(b>0)){
+            this.A=a;
+            this.B=b;
+        }
+        else{
+            throw new IllegalArgumentException("error Rectangle");
+        }
     }
     public double area(){
         return(A*B);
     }
+    public void show(){
+
+    }
 }
 class Square extends Rectangle{
-    double A;
-    public void create_square(double a){
-        A=a;
+    public Square(double as){
+        if (as>0){
+            this.A=as;
+            this.B=as;
+        }
+        else{
+            throw new IllegalArgumentException("error Sqare");
+        }
     }
-    public double area(){
-        return(A*A);
+    public void show(){
+
     }
 }
--- 4semestr/neironki/lab1.py
+++ 4semestr/neironki/lab1.py
@@ -2,23 +2,7 @@ from cmath import*
 import matplotlib.pyplot as plt
 import numpy as np
 
-def drawGraph():
-    plt. scatter([x1[i] for i in range(4)],[x2[i] for i in range(4)])
-    plt.xlabel("x1")
-    plt.ylabel("x2")
-    for i in range(4):
-        x,y = x1[i], x2[i]
-        kx = -1 if x == 0 else 4
-        ky = -1 if y == 0 else 4
-        plt.text(x - 0.02 * kx, y - 0.02 * ky, f"({x},{y})")
-    xx = [x1[i] for i in range(4)]
-    yy=[(-(w[1]/w[2])*x1[i]+(0.5-w[0]*x0[i])/w[2]) for i in range(4)]
-    print(xx)
-    print(yy)
-    plt.plot(xx, yy)
-    plt.savefig("and_graph.png")
-
-ly=0.0001
+ly=0.01
 w=[0.1,-0.05,0.05]
 x0=[1,1,1,1]
 x1=[0,0,1,1]
@@ -44,5 +28,22 @@ for epoh in range(1000):
 for sample in range(4):
     y=float(w[0])*float(x0[sample])+float(w[1])*float(x1[sample])+float(w[2])*float(x2[sample])
     print(c[sample],"   ",y)
+def graf():
+    plt. scatter([x1[i] for i in range(4)],[x2[i] for i in range(4)])
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    for i in range(4):
+        x,y = x1[i], x2[i]
+        kx = -1 if x == 0 else 4
+        ky = -1 if y == 0 else 4
+        plt.text(x - 0.02 * kx, y - 0.02 * ky, f"({x},{y})")
+    xx = [x1[i] for i in range(4)]
+    yy=[(-(w[1]/w[2])*x1[i]+(0.5-w[0]*x0[i])/w[2]) for i in range(4)]
+    print(xx)
+    print(yy)
+    plt.plot(xx, yy)
 
-drawGraph()
+    plt.savefig("and_graph.png")
+
+
+graf()
